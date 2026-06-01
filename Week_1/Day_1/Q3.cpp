a50434dd82d6d0431acff4ad6f27606357c25082
anshika solanki <solankianshi21@gmail.com>
2026-06-01 17:45:51 +0530

    Q3.cpp
    BEST TIME SELL AND BUY STOCK

--- Week_1/Day_1/Q3.cpp
+++ Week_1/Day_1/Q3.cpp
@@ -1,1 +1,9 @@
-
+class Solution {
+public:
+    int maxProfit(vector<int>& p) {
+        int mn = INT_MAX, ans = 0;
+        for(int x:p){ mn=min(mn,x); ans=max(ans,x-mn); }
+        return ans;
+        
+    }
+};
