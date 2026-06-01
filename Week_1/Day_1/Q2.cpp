910749e2e4425b27e662535c7eea155f4e91b529
anshika solanki <solankianshi21@gmail.com>
2026-06-01 17:44:20 +0530

    Update Q2.cpp
    REMOVE DUPLICATES

--- Week_1/Day_1/Q2.cpp
+++ Week_1/Day_1/Q2.cpp
@@ -1,1 +1,10 @@
-
+class Solution {
+public:
+    int removeDuplicates(vector<int>& a) {
+        int i = 0;
+        for(int j = 0; j < a.size(); j++)
+         if(a[j]!=a[i]) a[++i] = a[j];
+        return i+1;
+        
+    }
+};
