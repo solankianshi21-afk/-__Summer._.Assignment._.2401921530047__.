de790eaae916e0b55e3c14a842cacd5fcf39b85c
anshika solanki <solankianshi21@gmail.com>
2026-06-02 18:45:52 +0530

    Update Q_1.cpp

--- Week_1/Day_2/Q_1.cpp
+++ Week_1/Day_2/Q_1.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        long long maxi = LLONG_MIN;
+        long long sum = 0;
+        for(int i = 0; i < n; i++) {
+            sum += nums[i];
+            maxi = max(maxi, sum);
+            if( sum < 0) {
+                sum = 0;
+            }
+        }
+        return maxi;
+        
+    }
+};
