8cac5a09266c71a9f10de000303c4432992e7ee5
anshika solanki <solankianshi21@gmail.com>
2026-06-02 19:04:23 +0530

    Update Q_3.cpp

--- Week_1/Day_2/Q_3.cpp
+++ Week_1/Day_2/Q_3.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n = nums.size();
+        double sum = 0;
+        for(int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        double maxi = sum;
+        for(int i = k; i < n; i++) {
+            sum += nums[i] - nums[i - k];
+            maxi = max(sum, maxi);
+        }
+        return maxi/k;
+    }
+};
