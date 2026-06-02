21ed6ccc661b59ab0d86531484a40a67c58fddc9
anshika solanki <solankianshi21@gmail.com>
2026-06-02 18:47:06 +0530

    Update Q_2.cpp

--- Week_1/Day_2/Q_2.cpp
+++ Week_1/Day_2/Q_2.cpp
@@ -1,1 +1,9 @@
-
+lass Solution {
+public:
+    bool containsDuplicate(vector<int>& a) {
+        unordered_set<int>s;
+        for(int x:a) if(!s.insert(x).second) return true;
+        return false;
+        
+    }
+};
