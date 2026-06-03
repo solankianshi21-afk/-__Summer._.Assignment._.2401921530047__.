bf0a77c4a7019f3675fb08585a42356508f36356
anshika solanki <solankianshi21@gmail.com>
2026-06-03 22:54:00 +0530

    Update Q2.cpp

--- Week_1/Day_3/Q2.cpp
+++ Week_1/Day_3/Q2.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> a(nums.size());
+        int  k = 0;
+        for(int n : nums) {
+            a[k++] = n*n;
+        }
+        for(int s = 0; s < a.size(); s++) {
+            for(int i = s + 1; i < a.size(); i++) {
+                if(a[s] > a[i]) {
+                    int temp = a[s];
+                    a[s] = a[i];
+                    a[i] = temp;
+                }
+            }
+        }
+        return a;  
+    }
+};
