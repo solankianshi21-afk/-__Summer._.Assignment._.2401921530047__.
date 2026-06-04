3a100b0ea8e9c656a7cdfa2150a3ab790dd34e58
anshika solanki <solankianshi21@gmail.com>
2026-06-04 18:49:50 +0530

    Update Q2.cpp

--- Week_1/Day_4/Q2.cpp
+++ Week_1/Day_4/Q2.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        vector<vector<int>> vis(r, vector<int>(c, 0));
+        int n = mat.size();
+        int m = mat[0].size();
+        if(n*m != r*c) {
+            return mat;
+        }
 
+        int row = 0;
+        int col = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                vis[row][col] = mat[i][j];
+                col++;
+
+                if(col == c) {
+                    col = 0;
+                    row++;
+                }
+            }
+        }
+        return vis;
+        
+    }
+};
