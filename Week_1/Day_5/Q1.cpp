7170289c879d057ed3ce4a79e41b857aaf641e79
anshika solanki <solankianshi21@gmail.com>
2026-06-04 22:53:59 +0530

    Update Q1.cpp

--- Week_1/Day_5/Q1.cpp
+++ Week_1/Day_5/Q1.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+    bool inValid(char ch){
+        if((ch>='A' && ch<='Z') || (ch>='0' && ch<='9') || (ch>='a' && ch<='z')) return false;
+        return true;
+    }
 
+    bool isPalindrome(string s) {
+        int n=s.size();
+        int l=0;
+        int r=n-1;
+        while(l<r){
+            while(l<r && inValid(s[l])) l++;
+            while(l<r && inValid(s[r])) r--;
+            if(tolower(s[l])!=tolower(s[r])) return false;
+            l++;
+            r--;
+
+
+        }
+        return true;
+        
+    }
+};
