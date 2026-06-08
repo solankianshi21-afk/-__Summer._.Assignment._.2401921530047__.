f067dd49b9144878e4aef59217031960946615db
anshika solanki <solankianshi21@gmail.com>
2026-06-08 18:25:12 +0530

    Update Q1.cpp

--- Week2/Day_1/Q1.cpp
+++ Week2/Day_1/Q1.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+        int charCounts[26] = {0};
+        for(char c : s) {
+            charCounts[c - 'a']++; 
+        }
+        for(char c : t) {
+                charCounts[c - 'a']--;
+            if(charCounts[c - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
