fb0cdf28f15f6a8ffe273ead5bae2bc708d7ed6a
anshika solanki <solankianshi21@gmail.com>
2026-06-08 18:26:13 +0530

    Update Q3.cpp

--- Week2/Day_1/Q3.cpp
+++ Week2/Day_1/Q3.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int charCounts[26] = {0};
+        for(char c : magazine) {
+            charCounts[c - 'a']++; 
+        }
+        for(char c : ransomNote) {
+                charCounts[c - 'a']--;
+            if(charCounts[c - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+};
