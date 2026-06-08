72a7a6a305d53160e38bb976f640cb08c975e2cf
anshika solanki <solankianshi21@gmail.com>
2026-06-08 16:34:45 +0530

    Update Q3.cpp

--- Week_1/Day_5/Q3.cpp
+++ Week_1/Day_5/Q3.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
 
+        string b = strs[0];
+        
+        for (int i = 0; i < b.size(); i++) {
+            char ch = b[i];
+            
+            for (int j = 1; j < strs.size(); j++) {
+                
+                if (i >= strs[j].size() || strs[j][i] != ch) {
+                    return b.substr(0, i);
+                }
+            }
+        }
+        
+        return b;
+    }
+        
+};
