4651425a1fc61be71992cbd8e56b8de331eac23a
anshika solanki <solankianshi21@gmail.com>
2026-06-09 13:59:28 +0530

    Update Q1.cpp

--- Week2/Day_2/Q1.cpp
+++ Week2/Day_2/Q1.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if(s.empty()) return 0;
+        int maxi = INT_MIN;
+        int l = s.length();
+        for(int i = 0; i < l; i++) {
+            int k = 0;
+            string ans = "";
 
+            for(int j = i; j < l; j++) {
+                if(ans.find(s[j]) != string::npos) {
+                    break;
+                }
+                ans += s[j];
+                k++;
+                maxi = max(k, maxi);
+            }
+        }
+        return maxi;
+    }
+};
