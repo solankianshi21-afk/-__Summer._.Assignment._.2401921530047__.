36098d1154829944091f281e0aca1c38377d99f6
anshika solanki <solankianshi21@gmail.com>
2026-06-10 15:16:54 +0530

    Update Q3.cpp

--- Week2/Day_3/Q3.cpp
+++ Week2/Day_3/Q3.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n = s.length();
+        
+        for (int l = 1; l <= n / 2; l++) {
 
+            if (n % l == 0) {
+           
+                string sub = s.substr(0, l);
+                string constructed = "";
+                
+
+                int repetitions = n / l;
+                for (int i = 0; i < repetitions; i++) {
+                    constructed += sub;
+                }
+                
+          
+                if (constructed == s) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+};
