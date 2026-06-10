f26cf97cbd7161678c0f57db74f66267c39793ba
anshika solanki <solankianshi21@gmail.com>
2026-06-10 14:57:48 +0530

    Update Q1.cpp

--- Week2/Day_3/Q1.cpp
+++ Week2/Day_3/Q1.cpp
@@ -1,1 +1,16 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int a = haystack.length();
+        int b = needle.length();
+        if(b > a) return -1;
 
+        for(int i = 0; i <= a - b; i++) {
+            if(haystack.substr(i, b) == needle) {
+                    return i;
+            }
+            
+        }
+        return -1;
+    }
+};
