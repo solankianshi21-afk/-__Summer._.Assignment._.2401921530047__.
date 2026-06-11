3977f6632563ae375572168406da86266e23c8bb
anshika solanki <solankianshi21@gmail.com>
2026-06-11 19:46:02 +0530

    Update Q2.cpp

--- Week2/Day_4/Q2.cpp
+++ Week2/Day_4/Q2.cpp
@@ -1,1 +1,36 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        int i = 0;
+        return helper(s, i);
+    }
+    string helper(const string& s, int& i) {
+        string result = "";
+        int num = 0;
+        while(i < s.length()) {
+            char ch = s[i];
+            if(isdigit(ch)) {
+                num = num * 10 + (ch - '0');
+                i++;
+            }
+            else if(ch == '[') {
+                i++;
+                string nestedstring = helper(s, i);
+                while(num > 0) {
+                   result += nestedstring;
+                   num--;
+                }
 
+            }
+            else if(ch == ']') {
+                i++;
+                return result;
+            }
+            else{
+                result += ch;
+                i++;
+            }
+        }
+        return result;
+    }
+};
