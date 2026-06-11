e0519ce854511456d86fb3efd7970a259432762f
anshika solanki <solankianshi21@gmail.com>
2026-06-11 11:47:38 +0530

    Update Q1.cpp

--- Week2/Day_4/Q1.cpp
+++ Week2/Day_4/Q1.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n = s.size();
+        int l = 0; 
+        
+        for(int r = 0; r <= n; r++) {
+            if(r == n || s[r] == ' ') {
+               int i = l, j = r - 1;
+               while(i < j) {
+                   swap(s[i] ,s[j]);
+                   i++;
+                   j--;
+               }
+               l = r + 1;
+            }
+        }
+        return s;
+    }
+};
