2c0dc31f1d6f54a0496e29b42c1ab86eb6ad6884
anshika solanki <solankianshi21@gmail.com>
2026-06-11 19:29:23 +0530

    Update Q3.cpp

--- Week2/Day_4/Q3.cpp
+++ Week2/Day_4/Q3.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    void backtrack(vector<string>& ans, string current_string, int open, int close, int n) {
+        if(current_string.length() == n * 2) {
+            ans.push_back(current_string);
+            return;
+        }
+        if(open < n) {
+            backtrack(ans, current_string + "(", open + 1, close, n);
+        }
+        if(close < open) {
+            backtrack(ans, current_string + ")", open, close + 1, n);
+        }
 
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        backtrack(ans, "", 0, 0, n);
+        return ans;
+        
+    }
+};
