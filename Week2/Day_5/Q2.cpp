5fae3b5c6d2e54f0a26aaae5ea1dae534d75d050
anshika solanki <solankianshi21@gmail.com>
2026-06-12 23:22:37 +0530

    Update Q2.cpp

--- Week2/Day_5/Q2.cpp
+++ Week2/Day_5/Q2.cpp
@@ -1,1 +1,25 @@
-
+lass Solution {
+public:
+    int compress(vector<char>& chars) {
+        int ans = 0;
+        for(int i = 0; i < chars.size();) {
+            int count = 1; 
+            int j = i+1;
+            
+            while(j < chars.size() && chars[i] == chars[j] ) {
+                count++;
+                j++;
+            }
+            chars[ans++] = chars[i];
+            if(count > 1) {
+                string countStr = to_string(count);
+                for(char ch : countStr) {
+                    chars[ans++] = ch;
+                }
+            }
+            i = j;
+            
+        }
+        return ans;
+    }
+};
