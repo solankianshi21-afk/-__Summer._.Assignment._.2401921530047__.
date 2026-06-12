73770e041ffe261b2904cb65af7d783185205558
anshika solanki <solankianshi21@gmail.com>
2026-06-12 22:55:11 +0530

    Update Q1.cpp

--- Week2/Day_5/Q1.cpp
+++ Week2/Day_5/Q1.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> anagram;
+        for(string s : strs) {
+            vector<int> count(26, 0);
+            for(char c : s) {
+                count[c - 'a']++;
+            }
+            string key = "";
+            for(int i = 0; i < 26; i++) {
+                key += to_string(count[i]) + "#";
+            }
+            anagram[key].push_back(s);
+        }
+        vector<vector<string>> result;
+        for(auto pair : anagram) {
+            result.push_back(pair.second);
+        }
+        return result;
+    }
+};
