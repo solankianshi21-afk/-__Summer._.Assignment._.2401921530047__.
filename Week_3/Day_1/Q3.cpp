61dd1895dc8da2c3329cf40d1be15a50f05bae63
anshika solanki <solankianshi21@gmail.com>
2026-06-15 16:26:49 +0530

    Update Q3.cpp

--- Week_3/Day_1/Q3.cpp
+++ Week_3/Day_1/Q3.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if(head == 0) return nullptr;
+        ListNode* temp = head;
+        int count = 0;
+        while( temp != nullptr)  {
+            count++;
+            temp = temp->next;
+        }
+        int middposition = (count)/2;
+        ListNode*  middNode = head;
+        for(int i = 0; i < middposition; i++)    {
+            middNode = middNode->next;
+        }
+        return middNode;        
+        
+    }
+};
