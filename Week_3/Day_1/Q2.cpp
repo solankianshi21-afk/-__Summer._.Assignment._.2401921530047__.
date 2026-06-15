c305e4d4a7a19e0da731bd03f97b91877c02406d
anshika solanki <solankianshi21@gmail.com>
2026-06-15 16:25:34 +0530

    Update Q2.cpp

--- Week_3/Day_1/Q2.cpp
+++ Week_3/Day_1/Q2.cpp
@@ -1,1 +1,16 @@
 
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* temp = head;
+        ListNode* prev = nullptr;
+        while(temp != nullptr) {
+            ListNode* front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+
+        }
+        return prev;
+    }
+};
