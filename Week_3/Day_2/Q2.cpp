c9c3a1e3030864c39224ff09db174ce18491727e
anshika solanki <solankianshi21@gmail.com>
2026-06-16 13:30:37 +0530

    Update Q2.cpp

--- Week_3/Day_2/Q2.cpp
+++ Week_3/Day_2/Q2.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* fastp = head;
+        ListNode* slowp = head;
+        for(int i = 0; i < n; i++) {
+            fastp = fastp->next;
+        }
+        if(fastp == nullptr) {
+            return head->next;
+        }
+        while(fastp->next != nullptr) {
+            fastp = fastp->next;
+            slowp = slowp->next;
+        }
+        ListNode* delNode = slowp->next;
+        slowp->next = slowp->next->next;
+        delete delNode;
+        return head;
+        
+    }
+};
