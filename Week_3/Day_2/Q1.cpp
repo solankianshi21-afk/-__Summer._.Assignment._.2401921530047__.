11dcd7f9af3a23ecb2b62756b97baa9d38c0163c
anshika solanki <solankianshi21@gmail.com>
2026-06-16 13:28:57 +0530

    Update Q1.cpp

--- Week_3/Day_2/Q1.cpp
+++ Week_3/Day_2/Q1.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        vector<int> arr;
+        ListNode* temp1 = list1;
+        ListNode* temp2 = list2;
+        while(temp1 != NULL) {
+            arr.push_back(temp1->val);
+            temp1 = temp1->next;
+        }
+        while(temp2 != NULL) {
+            arr.push_back(temp2->val);
+            temp2 = temp2->next;
+        }
+        sort(arr.begin(), arr.end());
+        ListNode* dNode = new ListNode(-1);
+        ListNode* temp = dNode;
+        for(int i = 0; i < arr.size(); i++) {
+            temp->next = new ListNode(arr[i]);
+            temp = temp->next;
+        }
+        return dNode->next;
 
+    }
+};
