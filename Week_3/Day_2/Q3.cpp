5a7507b13805f70e07eb9e9dd3c18458726a3232
anshika solanki <solankianshi21@gmail.com>
2026-06-16 13:36:44 +0530

    Update Q3.cpp

--- Week_3/Day_2/Q3.cpp
+++ Week_3/Day_2/Q3.cpp
@@ -1,1 +1,66 @@
+class Solution {
+private:
+    
+    ListNode* reverseLinkedList(ListNode* head) {
+        
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        while (curr != NULL) {
+            
+            ListNode* nextNode = curr->next;
+      
+            curr->next = prev;
+            
+            prev = curr;
+            
+            curr = nextNode;
+        }
+        
+        return prev;
+    }
+
+public:
+    bool isPalindrome(ListNode* head) {
+        
+        if (head == NULL || head->next == NULL) {
+          
+            return true; 
+        }
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while (fast->next != NULL && fast->next->next != NULL) {
+        
+            slow = slow->next;  
+
+            fast = fast->next->next;  
+        }
+        
+        ListNode* newHead = reverseLinkedList(slow->next);
+        
+        ListNode* first = head;  
+ 
+        ListNode* second = newHead; 
+        
+        while (second != NULL) {
+        
+            if (first->val != second->val) {
+            
+                reverseLinkedList(newHead);  
+                
+                return false;
+            }
+            
+           
+            first = first->next; 
+            second = second->next;  
+        }
+        
+        
+        reverseLinkedList(newHead);  
+        
+        return true;  
+    }
+};
 
