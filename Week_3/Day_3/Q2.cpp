9f5157123d6d7e2bd412dc9e6675e6ff665ab381
anshika solanki <solankianshi21@gmail.com>
2026-06-17 13:34:59 +0530

    Update Q2.cpp

--- Week_3/Day_3/Q2.cpp
+++ Week_3/Day_3/Q2.cpp
@@ -1,1 +1,23 @@
+class MinStack {
+public: 
+ stack<int>s, ms;
 
+  MinStack() {}
+
+ void push(int val) {
+    s.push(val); 
+     if(ms.empty()||val<=ms.top())
+      ms.push(val);
+ }
+ void pop(){ 
+    if(s.top() == ms.top()) 
+     ms.pop(); 
+    s.pop();
+ }
+ int top() {
+    return s.top();
+ }
+ int getMin(){ 
+  return ms.top();
+  }
+};    
