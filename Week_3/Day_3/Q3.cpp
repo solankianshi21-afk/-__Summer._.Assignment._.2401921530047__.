07f09fef930fe479bc2821e9d2dc19efac09a407
anshika solanki <solankianshi21@gmail.com>
2026-06-17 13:36:35 +0530

    Update Q3.cpp

--- Week_3/Day_3/Q3.cpp
+++ Week_3/Day_3/Q3.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> st;
+        int n = nums2.size();
+        vector<int> nge(n);
+        for(int i = n - 1; i >= 0; i--) {
+            while(!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
+            }
+            if(st.empty()) nge[i] = -1;
+            else nge[i] = st.top();
 
+            st.push(nums2[i]);
+        }
+        unordered_map<int, int>mp;
+        for(int i = 0; i < n; i++) {
+            mp[nums2[i]] = nge[i];
+        }
+        vector<int> ans;
+        for(int x: nums1) {
+            ans.push_back(mp.count(x) ? mp[x] : -1);
+        }
+        return ans;
+    }
+};
