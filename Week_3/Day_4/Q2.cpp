093aa07b9753e1e39ad3d3d7bf3aa72cd1983d2d
anshika solanki <solankianshi21@gmail.com>
2026-06-18 16:45:56 +0530

    Update Q2.cpp

--- Week_3/Day_4/Q2.cpp
+++ Week_3/Day_4/Q2.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
 
+        for(string i : tokens){
+            if(i == "+" || i== "-" || i == "*" || i == "/"){
+                int b = st.top(); 
+                st.pop();
+
+                int a = st.top();
+                st.pop();
+
+                if(i == "+") st.push(a+b);
+                else if( i == "-") st.push(a-b);
+                else if( i== "*") st.push(a*b);
+                else st.push(a/b);
+            }
+            else {
+                st.push(stoi(i));
+            }
+        }
+        return st.top();
+    }
+};
