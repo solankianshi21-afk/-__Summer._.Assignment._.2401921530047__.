fc9e586f710c34252c913dadb40f69e6d85167e7
anshika solanki <solankianshi21@gmail.com>
2026-06-19 21:59:01 +0530

    Update Q1.cpp

--- Week_3/Day_5/Q1.cpp
+++ Week_3/Day_5/Q1.cpp
@@ -1,1 +1,37 @@
+class MyQueue {
+public:
+ stack<int>in, out;
+    MyQueue() {
 
+    }
+    void push(int x) {
+        in.push(x);    
+    }
+    
+    int pop() {
+        if(out.empty()) {
+          while(!in.empty()) {
+            out.push(in.top());
+            in.pop();
+          }
+        }
+        int front = out.top();
+        out.pop();
+        return front;
+    }
+    int peek() {
+        if(out.empty()) {
+            while(!in.empty()) {
+                out.push(in.top());
+                in.pop();
+            }
+        }
+        return out.top();
+    }
+
+    bool empty() {
+        return in.empty() && out.empty();
+    }
+    
+};
+    
