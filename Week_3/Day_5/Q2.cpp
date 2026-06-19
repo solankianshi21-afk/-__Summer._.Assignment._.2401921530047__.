c346e0c59718656b19e0fe3c5090ceb4b3e91d88
anshika solanki <solankianshi21@gmail.com>
2026-06-19 22:01:02 +0530

    Update Q2.cpp

--- Week_3/Day_5/Q2.cpp
+++ Week_3/Day_5/Q2.cpp
@@ -1,1 +1,13 @@
-
+class RecentCounter {
+public:
+    queue<int> q;
+    
+    int ping(int t) {
+        q.push(t);
+        while(q.front() < t-3000) 
+         q.pop();
+        return q.size();
+    }
+        
+    
+}
