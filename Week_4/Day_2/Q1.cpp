5e2d95efb54fddc3df05376ffc14fe03cd6e5da7
anshika solanki <solankianshi21@gmail.com>
2026-06-22 23:50:35 +0530

    Update Q1.cpp

--- Week_4/Day_2/Q1.cpp
+++ Week_4/Day_2/Q1.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    void func(TreeNode* Node, vector<int>& ans) {
+        if(Node == nullptr) {
+            return;
+        }
+        func(Node->left, ans);
+        ans.push_back(Node->val);
+        func(Node->right, ans);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        func(root, ans);
+        return ans;
+    }
+};
