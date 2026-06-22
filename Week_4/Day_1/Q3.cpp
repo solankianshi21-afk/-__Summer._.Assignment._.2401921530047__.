9c83187c1084822491a00d0b1ad034fdddcea04a
anshika solanki <solankianshi21@gmail.com>
2026-06-22 09:35:31 +0530

    Update Q3.cpp

--- Week_4/Day_1/Q3.cpp
+++ Week_4/Day_1/Q3.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
-    TreeNode* invertTree(TreeNode* root) {
-        if(root == nullptr) {
-            return root;
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == nullptr || q == nullptr) {
+            return (p == q);
         }
-        TreeNode* temp = root->left;
-        root->left = root->right;
-        root->right = temp;
-        invertTree(root->left);
-        invertTree(root->right);
-        return root;
+        return ((p->val == q->val) &&  isSameTree(p->left, q->left) 
+               && isSameTree(p->right,  q->right));
     }
 };
