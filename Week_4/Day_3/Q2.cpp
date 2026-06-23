1307be106ac529fb038fd99f9aefc46a88bea4d6
anshika solanki <solankianshi21@gmail.com>
2026-06-23 22:02:18 +0530

    Update Q2.cpp

--- Week_4/Day_3/Q2.cpp
+++ Week_4/Day_3/Q2.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
+    bool validate(TreeNode* node, long long minVal, long long maxVal) {
+        if (node == nullptr) return true;
+       
+        if (node->val <= minVal || node->val >= maxVal) return false;
+        
+    
+        return validate(node->left, minVal, node->val) && 
+               validate(node->right, node->val, maxVal);
+    }
 
+    bool isValidBST(TreeNode* root) {
+       
+        return validate(root, LLONG_MIN, LLONG_MAX);
+    }
+};
