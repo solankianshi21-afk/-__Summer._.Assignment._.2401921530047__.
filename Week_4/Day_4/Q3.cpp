b4de2ad104e75fc41a67f426717cb595786e4afa
anshika solanki <solankianshi21@gmail.com>
2026-06-24 22:00:49 +0530

    Update Q3.cpp

--- Week_4/Day_4/Q3.cpp
+++ Week_4/Day_4/Q3.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        int maxi = INT_MIN;
+        maxPathdown(root, maxi);
+        return maxi;
+    }
+private:
+    int maxPathdown(TreeNode* root, int &maxi) {
+        if(root == nullptr) {
+            return 0;
+        }
+        int left = max(0, maxPathdown(root->left, maxi));
+        int right = max(0, maxPathdown(root->right, maxi));
+        maxi = max(maxi, left+right+root->val);
+        return max(left, right) + root->val;
+    }
+};
