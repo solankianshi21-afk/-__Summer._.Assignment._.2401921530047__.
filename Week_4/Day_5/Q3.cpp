eb7fb5e9f1d89b4f6ce0d89e6e0c9e71d7155197
anshika solanki <solankianshi21@gmail.com>
2026-06-24 22:06:10 +0530

    Update Q3.cpp

--- Week_4/Day_5/Q3.cpp
+++ Week_4/Day_5/Q3.cpp
@@ -1,1 +1,71 @@
+class Codec {
+public:
 
+    string serialize(TreeNode* root) {
+         if (root == nullptr) {
+            return "";
+        }
+
+        
+        stringstream ss;
+       
+        queue<TreeNode*> q;
+       
+        q.push(root);
+
+        while (!q.empty()) {
+     
+            TreeNode* curNode = q.front();
+            q.pop();
+
+            if (curNode == nullptr) {
+                ss << "#,";
+            } else {
+                
+                ss << curNode->val << ",";
+             
+                q.push(curNode->left);
+                q.push(curNode->right);
+            }
+        }
+
+        return ss.str();
+    }
+    TreeNode* deserialize(string data) {
+       if (data.empty()) {
+            return nullptr;
+        }
+
+       
+        stringstream s(data);
+        string str;
+        getline(s, str, ',');
+       
+        TreeNode* root = new TreeNode(stoi(str));
+
+        queue<TreeNode*> q;
+    
+        q.push(root);
+
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            getline(s, str, ',');
+            if (str != "#") {
+                TreeNode* leftNode = new TreeNode(stoi(str));
+                node->left = leftNode;
+                q.push(leftNode);
+            }
+
+            getline(s, str, ',');
+            if (str != "#") {
+                TreeNode* rightNode = new TreeNode(stoi(str));
+                node->right = rightNode;
+                q.push(rightNode);
+            }
+        }
+
+        return root;
+    }
+};
