7ec5ce0a8a7abb32cd266f323441f45550d601cc
anshika solanki <solankianshi21@gmail.com>
2026-06-24 21:57:52 +0530

    Update Q1.cpp

--- Week_4/Day_4/Q1.cpp
+++ Week_4/Day_4/Q1.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (!root) {
+            return false;
+        }
+        
+        if (!root->left && !root->right) {
+            return root->val == targetSum;
+        }
+        
+        int remainingSum = targetSum - root->val;
+        return hasPathSum(root->left, remainingSum) || hasPathSum(root->right, remainingSum);
+    }
+};
 
+        
