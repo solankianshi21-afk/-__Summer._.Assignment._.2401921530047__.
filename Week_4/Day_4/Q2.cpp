73525fb4ebe70de513ed0356e2b202674ec7edb4
anshika solanki <solankianshi21@gmail.com>
2026-06-24 21:59:50 +0530

    Update Q2.cpp

--- Week_4/Day_4/Q2.cpp
+++ Week_4/Day_4/Q2.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int dia = 0;
+        height(root, dia);
+        return dia;
+        
+    }
+private:
+    int height(TreeNode* node, int& dia) {
+        if(!node) {
+            return 0;
+        }
+        int lh = height(node->left, dia);
+        int rh = height(node->right, dia);
+        dia = max(dia, lh + rh);
+        return 1 + max(lh , rh);
+    }
+};
